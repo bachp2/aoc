ef0f20af3a06c7b3cfb115394cf1caaf728a68db
Bach Phan <bachp2@uw.edu>
2017-12-23 15:44:13 -0800

    Initial Commit

---
+++ advent_of_code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  advent_of_code
+//
+//  Created by Bach Phan on 12/23/17.
+//  Copyright © 2017 Bach Phan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
